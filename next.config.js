eddb87f52fc3096429b2300cfd3e2103880dc451
Mirza <muzammil@spiralyze.com>
2026-01-04 20:16:09 +0800

    Remove base path configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/muzammilmirzaresume' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/muzammilmirzaresume' : ''
+  }
 }
 
 module.exports = nextConfig
